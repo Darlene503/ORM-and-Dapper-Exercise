859ade0df9c6e38e5262a4fb1191a4bb051ed74b
Darlene503 <duyn503@gmail.com>
2023-08-21 20:43:03 -0700

    recieving an omr error to fix

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Transactions;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var depRepo = new DepartmentRepo(conn);
+
+            Console.WriteLine("Enter as new department name: ");
+
+            var depName = Console.ReadLine();
+
+            depRepo.InsertDepartment(depName);
+
+            var departments = depRepo.GetAllDepartments();
+
+            foreach(var dep in departments) 
+            {
+                Console.WriteLine($"ID: {dep.DepartmentID} | Name: {dep.Name}");
+            }
+
+
         }
+
     }
 }
